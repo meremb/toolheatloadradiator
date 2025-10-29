b88b7a3c344350adf1e3b76d66c8b5470a70b140
meremb <meremabart@gmail.com>
2025-10-29 12:10:59 +0100

    chore: removed vercel wsgi

--- api/index.py
+++ api/index.py
@@ -1,5 +1,27 @@
-from app import server
-from vercel_wsgi import handle
+from dash import Dash, html
+from flask import Flask, request
+from werkzeug.middleware.proxy_fix import ProxyFix
+from werkzeug.test import EnvironBuilder
+from werkzeug.wrappers import Response
 
-def handler(request, response):
-    return handle(server, request, response)
+server = Flask(__name__)
+app = Dash(__name__, server=server)
+app.layout = html.Div([html.H1("Hello Dash on Vercel!")])
+
+def handler(event, context):
+    server.wsgi_app = ProxyFix(server.wsgi_app)
+    builder = EnvironBuilder(
+        path=event.get("path", "/"),
+        method=event.get("httpMethod", "GET"),
+        headers=event.get("headers", {}),
+        query_string=event.get("queryStringParameters"),
+        data=event.get("body", None),
+    )
+    env = builder.get_environ()
+    resp = Response.from_app(server.wsgi_app, env)
+    return {
+        "statusCode": resp.status_code,
+        "headers": dict(resp.headers),
+        "body": resp.get_data(as_text=True),
+    }
+
