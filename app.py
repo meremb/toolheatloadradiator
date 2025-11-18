523a07a5c0ead596339ba728159b50ebd14207aa
meremb <meremabart@gmail.com>
2025-11-18 13:00:04 +0100

    fix: fix import

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from dash import Dash, dcc, html, dash_table, Input, Output, State, no_update, c
 import plotly.express as px
 import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
-from simpleLoadModel import RoomLoadCalculator
+from utils.simpleLoadModel import RoomLoadCalculator
 from utils.helpers import (
     POSSIBLE_DIAMETERS, Radiator, Circuit, Collector, Valve,
     validate_data, calculate_weighted_delta_t
